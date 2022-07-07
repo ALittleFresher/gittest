6ac00cc6db0b87ab0ac6e1d5b7ccb2d5eeaaee4e
ALittleFresher <seu_zc19@163.com>
2022-07-07 14:06:54 +0800

    fibo修改

--- test2.go
+++ test2.go
@@ -7,10 +7,12 @@ func main() {
 }
 
 func fibo(res int) int {
-	//var res int
+	var j int = 1
 	for i := 0; i < 10; i++ {
-		res = i + res
-		fmt.Printf("第%d次叠加的值：%d", i, res)
+		mid := res
+		res = res + j
+		j = mid
+		fmt.Printf("第%d次叠加的值：%d\n", i, res)
 	}
 	return 0
 }
